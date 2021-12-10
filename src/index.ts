cd5026e68d6d7f37682995e0de1fe5a6e12a77af
sergTesst <sergiycheck@gmail.com>
2021-12-10 16:40:20 +0200

    repository

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,21 @@ import { Photo } from "./entity/Photo";
 
 createConnection()
   .then(async (connection) => {
-    const photos = await connection.manager.find(Photo);
-    if (!photos.length) {
-      const photo = new Photo(
-        "User 1 name",
-        "I am near polar bears",
-        "photo-with-bears.jpg",
-        1,
-        true
-      );
-      const savedResult = await connection.manager.save(photo);
+    const photoRepository = connection.getRepository(Photo);
+
+    const photo = new Photo(
+      "User 1 name",
+      "I am near polar bears",
+      "photo-with-bears.jpg",
+      1,
+      true
+    );
+
+    const photoExists = await photoRepository.findOne({ name: photo.name });
+    console.log("photoExists from db", photoExists);
+
+    if (!photoExists) {
+      const savedResult = await photoRepository.save(photo);
       console.log("savedResult ", savedResult);
       console.log("Photo has been saved. Photo id is ", photo.id);
     }
