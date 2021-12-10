7ef23b860744f2f10d22014e80ce490dc8a0c5fa
sergTesst <sergiycheck@gmail.com>
2021-12-10 19:06:09 +0200

    query builder

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,62 @@ import { createConnection } from "typeorm";
 import { Photo } from "./entity/Photo";
 import { PhotoMetadata } from "./entity/PhotoMetadata";
 
+type Connection = Awaited<ReturnType<typeof createConnection>>;
+
 createConnection()
   .then(async (connection) => {
-    const photoRepository = connection.getRepository(Photo);
-    const photoMetadataRepository = connection.getRepository(PhotoMetadata);
-
-    const photoWithRelation = new Photo(
-      "Me and wolfs",
-      "I am near wall",
-      "photo-with-snotes.jpg",
-      1,
-      true
-    );
-
-    const metadata = new PhotoMetadata(
-      640,
-      480,
-      "portrait",
-      true,
-      "amazing photo",
-      photoWithRelation
-    );
-
-    // //first we should save a photo
-    await photoRepository.save(photoWithRelation);
-
-    // //when the photo is saved we can save a photo metadata
-    await photoMetadataRepository.save(metadata);
-
-    console.log("all photos", await photoRepository.find());
-    console.log("all photo metadatas", await photoMetadataRepository.find());
+    await loadingObjectsWithTheirRelationsWithQueryBuilder(connection);
   })
   .catch((error) => console.log(error));
+
+async function loadingObjectsWithTheirRelationsWithQueryBuilder(
+  connection: Connection
+) {
+  const name = Photo.name.toLowerCase();
+
+  const photos = await connection
+    .getRepository(Photo)
+    .createQueryBuilder(name)
+    .innerJoinAndSelect("photo.metadata", "metadata")
+    .getMany();
+  console.log("photos with query builder", photos);
+}
+
+async function loadingObjectsWithTheirRelations(connection: Connection) {
+  const photoRepository = connection.getRepository(Photo);
+  const photos = await photoRepository.find({ relations: ["metadata"] });
+  console.log("photos ", photos);
+}
+
+async function storeRelationShipBetweenPhotoAndPhotoMedatada(
+  connection: Connection
+) {
+  const photoRepository = connection.getRepository(Photo);
+  const photoMetadataRepository = connection.getRepository(PhotoMetadata);
+
+  const photoWithRelation = new Photo(
+    "Me and wolfs",
+    "I am near wall",
+    "photo-with-snotes.jpg",
+    1,
+    true
+  );
+
+  const metadata = new PhotoMetadata(
+    640,
+    480,
+    "portrait",
+    true,
+    "amazing photo",
+    photoWithRelation
+  );
+
+  // //first we should save a photo
+  await photoRepository.save(photoWithRelation);
+
+  // //when the photo is saved we can save a photo metadata
+  await photoMetadataRepository.save(metadata);
+
+  console.log("all photos", await photoRepository.find());
+  console.log("all photo metadatas", await photoMetadataRepository.find());
+}
